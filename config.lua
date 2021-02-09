818813bdedd7c5668cb9e45daee83181932f8f81
Itz-Hyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-02-09 12:47:32 -0500

    changed this to remind people how to fucking format

--- config.lua
+++ config.lua
@@ -9,7 +9,7 @@ Config = {
 DiscordYeet = 'Sorry but this server reqiures you to have Discord (Desktop Edition) to play. Please restart FiveM and Discord if it is already open!', -- Message to client if their discord cannot be found by the script
 WhitelistYeet = 'Sorry but this is a whitelisted community if you would like to join please see our website- WEBSITE to apply!', -- Message to client if they are not whitelisted
 RoleIdsYeet = 'Sorry but I could not find your role ID\'s please reconnect and if this persists please contact the server developer!',
-WhitelistRoles = { -- Roles that you assigned in Badgers API that should pass the whitelist
+WhitelistRoles = { -- Roles that you assigned in Badgers API that should pass the whitelist, ALSO Make sure that **each** role ends with a comma!
     "Whitelisted",
     "MaybeWhitelisted",
     "ScrewItMakeThis",
